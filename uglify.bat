1d517c135962bbaf4aada71ac44d1d6c789352df
itsnoteasytonameaccount <whatasillyfishtwo@gmail.com>
2021-01-18 17:29:21 +0800

    modified:   uglify.bat

--- uglify.bat
+++ uglify.bat
@@ -1,4 +1,4 @@
 @echo off
-call terser scripts/background.js -m -o scripts/background.min.js --ie8
-call terser scripts/index.js -m -o scripts/index.min.js --ie8
+call terser scripts/background.js -m -o scripts/background.min.js
+call terser scripts/index.js -m -o scripts/index.min.js
 call node minify_html.js index.html
